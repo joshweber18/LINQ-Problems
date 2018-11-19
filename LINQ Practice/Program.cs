db37d9ba1433354911d14c91aa911d63a48f354f
joshweber18 <josh18weber@gmail.com>
2018-11-19 10:07:14 -0600

    th words completed

--- LINQ Practice/Program.cs
+++ LINQ Practice/Program.cs
@@ -8,8 +8,22 @@ namespace LINQ_Practice
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
+
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            var thword = words.Where(w => w.Contains("th"));
+
+            foreach (var word in thword)
+            {
+                Console.WriteLine(word);
+                Console.ReadLine();
+            }
+           
+
+
         }  
     }
 }
